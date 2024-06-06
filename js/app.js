35982b83531fcc7bfcfd12c7dc50a70ed63b15a6
Rogerio Lima <rlimapg@gmail.com>
2024-06-06 19:01:01 -0300

    Aluguel de Jogos

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+
+
+function alterarStatus(id){
+   let alugaGames = document.getElementById(`game-${id}`);
+   let imagem = alugaGames.querySelector('.dashboard__item__img');
+   let botao = alugaGames.querySelector('.dashboard__item__button');
+
+   if(imagem.classList.contains('dashboard__item__img--rented')){
+      imagem.classList.remove('dashboard__item__img--rented');
+      botao.classList.remove('dashboard__item__button--return');
+      botao.textContent = 'Alugar';
+   }else{
+      imagem.classList.add('dashboard__item__img--rented');
+      botao.classList.add('dashboard__item__button--return');
+      botao.textContent = 'Devolver';
+
+   }
+
+ 
+   
+   
+   
+}
